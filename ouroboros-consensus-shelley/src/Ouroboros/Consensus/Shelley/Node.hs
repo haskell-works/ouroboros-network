156d084232af59d47af3ed455bf67a9257248e1b
Thomas Winant <thomas@well-typed.com>
2020-11-17 16:37:11 +0100

    Add a pure Mary mode
    Note that this mode is not tested by consensus like the others, i.e., we only
    test the Mary era as part of the Cardano blockchain, not on its own.

--- ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Node.hs
+++ ouroboros-consensus-shelley/src/Ouroboros/Consensus/Shelley/Node.hs
@@ -19,6 +19,7 @@
 
 module Ouroboros.Consensus.Shelley.Node (
     protocolInfoShelley
+  , protocolInfoMary
   , protocolInfoShelleyBased
   , ProtocolParamsShelleyBased (..)
   , ProtocolParamsShelley (..)
@@ -246,6 +247,17 @@ protocolInfoShelley protocolParamsShelleyBased
                     ProtocolParamsShelley {
                         shelleyProtVer = protVer
                       } =
+    protocolInfoShelleyBased protocolParamsShelleyBased protVer
+
+protocolInfoMary ::
+     forall m c f. (IOLike m, ShelleyBasedEra (MaryEra c), Foldable f)
+  => ProtocolParamsShelleyBased (MaryEra c) f
+  -> ProtocolParamsMary
+  -> ProtocolInfo m (ShelleyBlock (MaryEra c))
+protocolInfoMary protocolParamsShelleyBased
+                 ProtocolParamsMary {
+                     maryProtVer = protVer
+                   } =
     protocolInfoShelleyBased protocolParamsShelleyBased protVer
 
 protocolInfoShelleyBased ::
